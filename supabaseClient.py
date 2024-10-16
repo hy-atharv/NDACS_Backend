f682db638f4edf4d20d270589706632f6a416a0c
Atharv Kumar Tiwari <131706297+hy-atharv@users.noreply.github.com>
2024-10-16 16:08:46 +0530

    Update supabaseClient.py

--- supabaseClient.py
+++ supabaseClient.py
@@ -1,7 +1,7 @@
 from supabase import create_client, Client
 
-url: str = "https://uevzxklfpoxxyosyugrs.supabase.co/"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVldnp4a2xmcG94eHlvc3l1Z3JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4NzM3NjYsImV4cCI6MjA0MDQ0OTc2Nn0.gL-BthHLI1t5fB_PcDBw1m1j4xpNA8yqesOMB20B4ZQ"
+url: str = "URL"
+key: str = "KEY"
 supabase: Client = create_client(url, key)
 
 
